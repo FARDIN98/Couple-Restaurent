a009230bce93f5c0550ad4d7e3c8d0d07b805181
aks2042 <freelancerakash3@gmail.com>
2021-06-04 00:11:29 +0600

    text add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ const Home = () => {
     return (
         <div>
             <h2>home</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis neque nulla eum placeat quaerat fugiat temporibus perferendis amet veritatis iste voluptate ab natus id esse quod cupiditate praesentium, aperiam aspernatur!</p>
+            <p>Lorem ipsum dolor sit amet.</p>
         </div>
     );
 };
